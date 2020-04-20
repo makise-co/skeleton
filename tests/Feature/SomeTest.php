0be362f70a2e77b22e86e4789ccb38c7a20d7ec6
coder <coder1994@gmail.com>
2020-04-20 20:53:31 +0300

    App skeleton update for newer framework version

---
+++ tests/Feature/SomeTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * File: SomeTest.php
+ * Author: Dmitry K. <dmitry.k@brainex.co>
+ * Date: 2020-04-20
+ * Copyright (c) 2020
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class SomeTest extends TestCase
+{
+    public function testOk(): void
+    {
+        $this
+            ->get('/')
+            ->assertStatus(200);
+    }
+}
